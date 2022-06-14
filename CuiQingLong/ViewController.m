eb6a2b67bbfb46887b4143038486fb53242c9d91
hzwangjianfei <hzwangjianfei@corp.netease.com>
2022-06-14 16:47:09 +0800

    增加动画

--- CuiQingLong/ViewController.m
+++ CuiQingLong/ViewController.m
@@ -13,6 +13,7 @@
 @property (nonatomic, strong) UILabel *time;
 @property (nonatomic, strong) NSArray *allWeibos;
 @property (nonatomic, strong) NSDictionary *current;
+@property (nonatomic, strong) UIView *coverView;
 
 @end
 
@@ -29,38 +30,42 @@
     NSError *error;
     self.allWeibos = [NSJSONSerialization JSONObjectWithData: data options:NSJSONReadingAllowFragments error:&error];
 
-    //    NSLog(@"%ld, %@", dic.count, error);
-
-
+//    NSLog(@"%ld, %@", dic.count, error);
+    
+    
     self.time = [UILabel new];
     [self.view addSubview:self.time];
-    self.time.frame = CGRectMake(24, 50, self.view.bounds.size.width-40, 10);
+    self.time.frame = CGRectMake(24, 43, self.view.bounds.size.width-40, 12);
     self.time.font = [UIFont systemFontOfSize:10 weight:UIFontWeightThin];
     self.time.textColor = [UIColor colorWithRed:0x88/255.0 green:0x88/255.0 blue:0x88/255.0 alpha:1];
-
+    
     UITextView *label = [UITextView new];
     [self.view addSubview:label];
     label.frame = CGRectMake(20, 60, self.view.bounds.size.width-40, self.view.bounds.size.height-120);
     self.label = label;
     self.label.attributedText = [self next];
-    //    self.label.userInteractionEnabled = NO;
+//    self.label.userInteractionEnabled = NO;
     self.label.backgroundColor = [UIColor colorWithRed:0x11/255.0 green:0x11/255.0 blue:0x11/255.0 alpha:1];
     self.label.editable = NO;
-
+    
     self.time.text = [self nextTime];
-
+    
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap)];
     [self.label addGestureRecognizer:tap];
+    
+    self.coverView = [[UIView alloc] initWithFrame:self.label.frame];
+    [self.view addSubview:self.coverView];
+    self.coverView.userInteractionEnabled = NO;
+    self.coverView.backgroundColor = [UIColor clearColor];
+}
 
-    }
-
-    - (NSParagraphStyle *)paragraphStyle {
+- (NSParagraphStyle *)paragraphStyle {
     NSMutableParagraphStyle * paragraphStyle = [[NSMutableParagraphStyle alloc] init];
     [paragraphStyle setLineSpacing:8];
     return paragraphStyle;
-    }
+}
 
-    - (NSAttributedString *)formated:(NSString *)text {
+- (NSAttributedString *)formated:(NSString *)text {
     text = [text stringByReplacingOccurrencesOfString:@"收起全文d" withString:@""];
     NSMutableAttributedString *attributeText = [[NSMutableAttributedString alloc] initWithString:text];
     [attributeText addAttribute:NSParagraphStyleAttributeName
@@ -73,30 +78,38 @@
                           value:[UIColor colorWithRed:0xdd/255.0 green:0xdd/255.0 blue:0xdd/255.0 alpha:1]
                           range:NSMakeRange(0, text.length)];
     return attributeText;
-    }
+}
 
 
-    - (NSDictionary *)findAWeibo {
+- (NSDictionary *)findAWeibo {
     int x = ABS(random())%self.allWeibos.count;
     self.current = [self.allWeibos objectAtIndex:x];
     return self.current;
-    }
+}
 
-    - (NSAttributedString *)next {
+- (NSAttributedString *)next {
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     NSDictionary *aWeibo = [self findAWeibo];
     self.label.contentOffset = CGPointMake(0, 0);
     return [self formated:aWeibo[@"发布内容"]];
-    }
+}
 
-    - (NSString *)nextTime {
+- (NSString *)nextTime {
     return [NSString stringWithFormat:@"%@, like %@, repost %@, comment %@", self.current[@"发布时间"], self.current[@"点赞数"], self.current[@"转发数"], self.current[@"评论数"]];
-    }
+}
 
-    - (void)tap {
-    self.label.attributedText = [self next];
-    self.time.text = [self nextTime];
-    }
+- (void)tap {
+    [UIView animateWithDuration:0.3 animations:^{
+        self.coverView.backgroundColor = [UIColor colorWithRed:0x11/255.0 green:0x11/255.0 blue:0x11/255.0 alpha:1];
+    } completion:^(BOOL finished) {
+        self.label.attributedText = [self next];
+        self.time.text = [self nextTime];
+        [UIView animateWithDuration:0.3 animations:^{
+            self.coverView.backgroundColor = [UIColor clearColor];
+        } completion:^(BOOL finished) {
+        }];
+    }];
+}
 
 
 @end
